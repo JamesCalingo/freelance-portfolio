2b3a80f405d88c392be935f48e4a68b543cd1d5c
JC <james.calingo@gmail.com>
2021-07-13 14:01:18 -0400

    adding svelte and starting

---
+++ src/App.svelte
@@ -0,0 +1,45 @@
+<script>
+
+</script>
+
+<main>
+<header>
+	<h1>James Calingo</h1>
+	<h2>Developing websites and applications for you.</h2>
+</header>
+	<div class="card">This card will be myw blurbs.</div>
+
+	
+</main>
+
+<style>
+	main {
+		text-align: center;
+		max-width: 240px;
+		margin: 0 auto;
+	}
+
+	header {
+		margin-bottom: 30px;
+		background: blue;
+	}
+
+	.card{
+		color: green;
+		font-size: 19pt;
+		border: 3px solid green
+	}
+
+	h1 {
+		color: #ff3e00;
+		text-transform: uppercase;
+		font-size: 4em;
+		font-weight: 100;
+	}
+
+	@media (min-width: 640px) {
+		main {
+			max-width: 85%;
+		}
+	}
+</style>
