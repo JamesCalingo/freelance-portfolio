63123262d238945d7a8f761f48b2ce064cfd4419
JC <james.calingo@gmail.com>
2021-07-14 11:04:45 -0400

    more design work

--- src/App.svelte
+++ src/App.svelte
@@ -1,66 +1,83 @@
 <script>
-
 </script>
 
 <main>
-<header>
-	<h1>James Calingo</h1>
-	<h2>Developing websites and applications for you.</h2>
-</header>
-	<div class="card">
-		This card will be myw blurbs
-	</div>
+  <header>
+    <h1>James Calingo</h1>
+    <h2>Developing websites and applications for you.</h2>
+  </header>
+  <div class="card">
+    <div class="headline">
+      Hi! I'm James Calingo, a full stack web developer with experience in
+      modern HTML, CSS, and JavaScript, as well as several frameworks.
+    </div>
+    <div class="bullets">
+			<div class="card">
+				<h1>card 1</h1>
+			</div>
+			<div class="card">
+				<h1>card 2</h1>
+			</div>
+			<div class="card">
+				<h1>card 3</h1>
+			</div>
+		</div>
+  </div>
 
-	<div class="card">
-		This area will be images of projects
-	</div>
+  <div class="card">This area will be images of projects</div>
 
-	<div class="card">
-		This will be testemonials
-	</div>
+  <div class="card">This will be testemonials</div>
 
-	<div class="card">
-	<label for="text">This will be a contact form</label>
-		<input type="text" placeholder="People can type stuff in here" />
-	</div>
+  <div class="card">
+    <label for="text">This will be a contact form</label>
+    <input type="text" placeholder="People can type stuff in here" />
+  </div>
 
-<p>Some ending stuff</p>
-	
+  <p>Some ending stuff</p>
 </main>
 
 <style>
-	main {
-		text-align: center;
-		max-width: 240px;
-		margin: 0 auto;
+  main {
+    text-align: center;
+    max-width: 240px;
+    margin: 0 auto;
+  }
+
+  header {
+    margin-bottom: 30px;
+    background: blue;
+  }
+
+  input {
+    width: 80%;
+  }
+
+  .card {
+    margin-bottom: 50px;
+    height: 30vh;
+    border: 3px solid green;
+    border-radius: 3px;
+  }
+
+	.bullets {
+		display: flex;
+		justify-content: space-evenly;
 	}
 
-	header {
-		margin-bottom: 30px;
-		background: blue;
+	.bullets .card {
+		height: 90%;
 	}
 
-input {
-	width: 80%;
-}
+  h1 {
+  color: green;
+    text-transform: uppercase;
+    font-size: 4em;
+    font-weight: 100;
+  }
 
-	.card{
-		margin-bottom: 50px;
-		height: 30vh;
-		border: 3px solid green;
-		border-radius: 3px;
-	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: 85%;
-		}
-	}
+  @media (min-width: 640px) {
+    main {
+      max-width: 85%;
+    }
+  }
 </style>
